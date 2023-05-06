8701357466d08f40465d165c2b4cc194662e54ec
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-05-06 14:26:52 -0300

    manifest & service worker

--- sw.js
+++ sw.js
@@ -1,29 +1,32 @@
-if ("serviceWorker" in navigator) {
-    window.addEventListener("load", function () {
-        navigator.serviceWorker
-            .register("./sw.js")
-            .then(res => console.log("service worker registrado", res))
-            .catch(err => console.log("service worker no registrado", err));
-    });
-}
+const cacheName = "v1";
 
-self.addEventListener('install', async e => {
-    console.log("Service Worker instalado");
+const cachedAssets = ["./", "./index.html", "./css/styles.css", "./html/app.html", "./js/app.js", "./tasks.json", "./imgs/png/"];
 
-    var cache = await caches.open("pwa-static");
-    cache.addAll([
-        "./",
-        "./index.html",
-        "./css/styles.css",
-        "./html/app.html",
-        "./js/app.js",
-        "./tasks.json",
-        "./imgs/png/",
-    ]);
-});
-
-self.addEventListener("activate", () => {
-    console.log("Service Worker activado");
+self.addEventListener("install", (e) => {
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then((cache) => {
+            cache.addAll(cachedAssets);
+        })
+        .then(() => self.skipWaiting())
+    );
 })
 
-self.addEventListener('fetch', () => console.log("fetch"));
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+})
