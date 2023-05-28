5a95b97779bf6bc77c607edab27d71338d68c6cd
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-05-28 14:44:30 -0300

    pending & done tasks

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ let modalAddNewList = document.getElementById("bg-modal-newlist");
 let formAddNewList = document.getElementById("addListForm");
 let closeModalAddNewList = document.getElementById("close-modal");
 let showOnDetail = document.querySelectorAll(".show-on-detail");
-// let divTasklistsContainer;
 let deleteTaskListButton;
 let taskListPage;
 
@@ -44,8 +43,9 @@ fetchTaskLists()
   .catch((json) => console.log(json));
 
 
-/* Función para mostrar el listado (Listas de tareas) */
+/* Mostrar el listado (Listas de tareas) */
 function renderTaskLists() {
+
   vaciarContainer();
 
   //Escondemos los elementos necesarios
@@ -53,7 +53,7 @@ function renderTaskLists() {
     element.classList.add("show-on-detail");
   });
 
-  // Cambiamos el contenido de la página
+  // Cambiamos el contenido del título de la página
   mainTitle.textContent = "Mis listas de tareas";
   mainSubtitle.innerHTML = `¡Mantené tus to-dos ordenados agrupándolos por listas! <br>
   Podés crear todas las que necesites.`;
@@ -62,7 +62,7 @@ function renderTaskLists() {
   let divTasklistsContainer = crearTasklistsContainer();
   appContentContainer.appendChild(divTasklistsContainer);
 
-  //Renderizamos las listas de tareas
+  //Creamos una card por cada lista de tareas
   arrayTaskLists.forEach((taskList) => {
     let cardList = `<button class="list" id="${taskList.id}">
                           <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="none" viewbox="0 0 48 48"
@@ -85,33 +85,30 @@ function renderTaskLists() {
     divTasklistsContainer.innerHTML += cardList;
   });
 
-  //Le agregamos a cada lista la función de eliminar
+  //Evento para eliminar una lista de tareas
   arrayTaskLists.forEach((taskList) => {
-    deleteTaskListButton = document.getElementById(
-      `delete-list-${taskList.id}`
-    );
+
+    deleteTaskListButton = document.getElementById(`delete-list-${taskList.id}`);
 
     deleteTaskListButton.addEventListener("click", function (e) {
-      // Identificamos la lista de tareas y la eliminamos del array
+    
+      //Identificamos la lista
       let idBoton = e.currentTarget.id;
+      let index = arrayTaskLists.findIndex((taskList) => `delete-list-${taskList.id}` === idBoton);
 
-      let index = arrayTaskLists.findIndex(
-        (taskList) => `delete-list-${taskList.id}` === idBoton
-      );
-      // console.log(index);
-
+      //La eliminamos del array
       arrayTaskLists.splice(index, 1);
       console.log(arrayTaskLists);
 
       renderTaskLists();
+
     });
   });
 
-  /* Le agregamos a cada lista la función para ver sus tareas */
+  //Evento para ver las tareas (Página de detalle)
   arrayTaskLists.forEach((taskList) => {
     taskListPage = document.getElementById(`${taskList.id}`);
-   
-    taskListPage.addEventListener("click", renderTasks)
+    taskListPage.addEventListener("click", renderTasks);
   });
 
   //Creamos y agregamos el botón "Agregar una nueva lista de tareas"
@@ -129,8 +126,6 @@ let vaciarContainer = function () {
 let crearTasklistsContainer = function () {
   let divTasklistsContainer = document.createElement("div");
   divTasklistsContainer.classList.add("lists-container");
-  // divTasklistsContainer.setAttribute("id", "divTasklistsContainer");
-
   return divTasklistsContainer;
 };
 
@@ -145,7 +140,7 @@ let crearButtonNewList = function () {
   </svg>
   <p>Agregar una nueva lista</p>`;
 
-  /* Evento para mostrar el formulario al hacerle click */
+  /* Evento para mostrar el formulario */
   button.addEventListener("click", function () {
     modalAddNewList.style.display = "grid";
     body.style.overflowY = "hidden";
@@ -169,31 +164,86 @@ window.addEventListener("click", function (event) {
 });
 
 
+/* Agregar una nueva lista de tareas */
+formAddNewList.addEventListener("submit", addNewList);
+
+function addNewList(e) {
+  e.preventDefault();
+
+  //Obtenemos los valores enviados por el form
+  let formName = document.getElementById("name");
+  let formDescription = document.getElementById("description");
+  let formType = document.querySelector('input[name="type"]:checked');
+
+  //Generamos un ID único (?)
+  let id = arrayTaskLists.length + 1;
+
+  //Creamos un objeto con los datos ingresados
+  let newList = {
+    id: id,
+    name: formName.value,
+    description: formDescription.value,
+    type: formType.value,
+    tasks: [],
+  };
+
+  //Agregamos el objeto al arrayTaskLists
+  arrayTaskLists.push(newList);
+
+  //Reseteamos los inputs del form
+  formName.value = '';
+  formDescription.value = '';
+  formType.checked = false;
+
+  hideModal();
+
+  renderTaskLists();
+
+}
 
 
-
-/* Función para ver las tareas (página de detalle) */
+/* Ver las tareas (Página de detalle) */
 function renderTasks(e){
 
-  // Identificamos la lista de tareas
+  // Identificamos la lista 
   let idList = e.currentTarget.id;
   let index = arrayTaskLists.findIndex((taskList) => `${taskList.id}` === idList);
 
+  // Guardamos en variables
   let selectedTaskList = arrayTaskLists[index];
-  console.log(selectedTaskList);
-
+  // console.log(selectedTaskList);
   let tasks = arrayTaskLists[index].tasks;
-  console.log(tasks);
+  // console.log(tasks);
 
-  // Vaciamos container
+
+  //Creamos 2 arrays nuevos a partir del array "tasks", usando el estado de sus tareas ("done_status")
+  let pendingTasks = [];
+  let doneTasks = [];
+
+  tasks.forEach((task) => {
+    if (task.done_status == false){
+      pendingTasks.push(task);
+    } else if (task.done_status == true){
+      doneTasks.push(task);
+    } else {
+      console.log(task);
+    }
+  });
+
   vaciarContainer();
 
-  // Cambiamos contenido títulos
+  // Agregamos en el nav la posibilidad de volver atrás
+  showOnDetail.forEach((element) => {
+    element.classList.remove("show-on-detail");
+  });
+  navGoBack.addEventListener("click", renderTaskLists);
+
+  // Cambiamos el contenido del título de la página
   mainTitle.textContent = selectedTaskList.name;
   mainSubtitle.textContent = selectedTaskList.description;
   listType.textContent = selectedTaskList.type;
 
-  // Agregamos contenido de la página de detalle
+  // Agregamos contenido propio de la página de detalle
   appContentContainer.innerHTML += 
   `<form action="#" id="addTaskForm">
     <input type="text" id="newTask" name="newTask" placeholder="Agregar una nueva tarea..." required>
@@ -211,7 +261,7 @@ function renderTasks(e){
 
   <div class="tasks-container" id="tasks-container"></div>`
 
-  // Seleccionamos los elementos necesarios para mostrar las tareas (por estado)
+  // Seleccionamos los elementos necesarios para mostrar las tareas (POR ESTADO)
   let tasksContainer = document.getElementById('tasks-container');
 
   let columnTasksPending = document.createElement('div');
@@ -222,6 +272,7 @@ function renderTasks(e){
   tasksContainer.appendChild(columnTasksPending);
   tasksContainer.appendChild(columnTasksDone);
 
+  //Columna "Tareas pendientes"
   columnTasksPending.innerHTML += 
   `
   <div class="tasks-info">
@@ -234,7 +285,7 @@ function renderTasks(e){
 
   let pendingTasksContainer = document.getElementById('tasksPending');
 
-  tasks.forEach((task) => {
+  pendingTasks.forEach((task) => {
     let taskItem =  
     `<div class="task">
       <input type="checkbox" id="${task.id}">
@@ -252,22 +303,39 @@ function renderTasks(e){
     pendingTasksContainer.innerHTML += taskItem
   });
 
+  //Columna "Tareas completadas"
+  columnTasksDone.innerHTML += 
+  `
+  <div class="tasks-info">
+    <h2 class="h3">Completadas</h2>
+    <span class="task-counter">3</span>
+  </div>
 
-  // tasks.forEach((task) => {
-   
-  // })
+  <div class="tasks" id="tasksDone"></div>
+  `;
 
-  //Cambiar visibilidad nav
+  let doneTasksContainer = document.getElementById('tasksDone');
 
-  //Cambiar visibilidad span type
+  doneTasks.forEach((task) => {
+    let taskItem =  
+    `<div class="task">
+      <input type="checkbox" id="${task.id}" checked>
+      <label for="${task.id}"><span class="custom-checkbox"></span>${task.task_name}</label>
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"
+          viewbox="0 0 48 48" class="delete-list delete-list-task">
+          <path fill="#949BA3"
+              d="M10.742 13.543c-.686 0-1.242.634-1.242 1.413 0 .78.556 1.413 1.24 1.413h26.518c.686 0 1.242-.633 1.242-1.413s-.556-1.413-1.244-1.413h-4.14c-.854-.024-1.756-.642-2.046-1.554l-.049-.161-.183-.63-.007-.021c-.111-.38-.209-.71-.345-1.006-.543-1.19-1.553-2.017-2.718-2.228-.294-.053-.607-.053-.966-.053h-5.604c-.357 0-.67 0-.966.053-1.165.211-2.174 1.038-2.718 2.228-.138.3-.236.635-.347 1.018l-.003.009-.185.63-.049.16c-.29.913-1.042 1.53-1.894 1.555h-4.294Z" />
+          <path fill="#949BA3" fill-rule="evenodd"
+              d="M24.65 40.112h-1.3c-4.485 0-6.726 0-8.184-1.427-1.457-1.425-1.606-3.766-1.904-8.441l-.43-6.757c-.163-2.54-.244-3.81.488-4.616.73-.805 1.966-.805 4.435-.805h12.49c2.47 0 3.704 0 4.435.805.73.804.65 2.076.488 4.616l-.43 6.756c-.298 4.676-.447 7.015-1.903 8.442-1.46 1.427-3.7 1.427-8.184 1.427Zm-4.797-16.945c.664-.07 1.257.44 1.321 1.139l.806 8.479c.066.7-.418 1.323-1.083 1.392-.664.07-1.257-.44-1.321-1.14l-.806-8.478c-.066-.7.418-1.323 1.083-1.392Zm9.377 1.392c.067-.7-.418-1.323-1.081-1.392-.663-.07-1.259.44-1.323 1.139l-.806 8.479c-.066.7.418 1.323 1.083 1.392.662.07 1.257-.44 1.321-1.14l.806-8.478Z"
+              clip-rule="evenodd" />
+      </svg>
+    </div>`
 
-  //Cambiar titulo y descripcion
-
-  showOnDetail.forEach((element) => {
-    element.classList.remove("show-on-detail");
+    doneTasksContainer.innerHTML += taskItem
   });
 
-  navGoBack.addEventListener("click", renderTaskLists)
+
+
 
 }
  
@@ -276,46 +344,6 @@ function renderTasks(e){
 
 
 
-/* Agregar una nueva lista de tareas */
-formAddNewList.addEventListener("submit", addNewList);
-
-console.log(arrayTaskLists)
-
-function addNewList(e) {
-  e.preventDefault();
-
-  //Obtenemos los valores enviados del form
-  let formName = document.getElementById("name");
-  let formDescription = document.getElementById("description");
-  let formType = document.querySelector('input[name="type"]:checked');
-
-  //Generamos un ID único (?)
-  let id = arrayTaskLists.length + 1;
-  console.log(id);
-
-  //Creamos un objeto con los datos ingresados
-  let newList = {
-    id: id,
-    name: formName.value,
-    description: formDescription.value,
-    type: formType.value,
-    tasks: [],
-  };
-
-  //Agregamos el objeto al arrayTaskLists
-  arrayTaskLists.push(newList);
-  // console.log(arrayTaskLists)
-
-  //Reseteamos los inputs del form
-  formName.value = '';
-  formDescription.value = '';
-  formType.checked = false;
-
-  hideModal();
-
-  renderTaskLists();
-
-}
 
 
 
