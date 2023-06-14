0cb87e9d7d5d91fab40561c1c3bdb0fa49d9d1b7
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-06-14 00:34:28 -0300

    estrategia de caché

--- sw.js
+++ sw.js
@@ -46,3 +46,27 @@ self.addEventListener("activate", (e) => {
 self.addEventListener("fetch", (e) => {
     e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
 })
+
+//Estrategia de caché (Cache first, falling back to network)
+self.addEventListener('fetch', (event) => {
+ 
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      // Go to the cache first
+      return cache.match(event.request.url).then((cachedResponse) => {
+        // Return a cached response if we have one
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        // Otherwise, hit the network
+        return fetch(event.request).then((fetchedResponse) => {
+          // Add the network response to the cache for later visits
+          cache.put(event.request, fetchedResponse.clone());
+
+          // Return the network response
+          return fetchedResponse;
+        });
+      });
+    }));
+ 
+});
