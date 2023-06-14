84e90a7ab41b96a841c56442ea206d6d7cf511c4
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-06-13 21:36:11 -0300

    función para borrar tarea en específico

--- js/detail-page.js
+++ js/detail-page.js
@@ -2,6 +2,7 @@
 let mainTitle = document.getElementById("main-title");
 let mainSubtitle = document.getElementById("main-subtitle");
 let listType = document.getElementById("list-type");
+let addNewTaskForm = document.getElementById("addTaskForm");
 let totalCount = document.getElementById("total-count");
 let pendingCount = document.getElementById("pending-count");
 let doneCount = document.getElementById("done-count");
@@ -135,6 +136,9 @@ function renderTaskCounters(){
 /* Función para renderizar las tareas (por estado: pendientes y completadas) */
 function renderTasks(){
 
+  pendingTasksContainer.innerHTML = '';
+  doneTasksContainer.innerHTML = '';
+
   const transaction = db.transaction(['toDoTasks'], 'readonly');
   let objectStore = transaction.objectStore('toDoTasks');
   let listAndStatusIndex = objectStore.index("done_status_and_list");
@@ -145,7 +149,6 @@ function renderTasks(){
   pendingTasksRequest.onsuccess = function(e) {
 
     let pendingTasks = e.target.result;
-    console.log(pendingTasks);
 
     if (pendingTasks.length === 0){
         pendingTasksContainer.innerHTML = "<p>¡No hay tareas pendientes!</p>"
@@ -168,6 +171,11 @@ function renderTasks(){
         pendingTasksContainer.innerHTML += taskItem
       });
     }
+
+    pendingTasks.forEach((task) => {
+      let svgDeleteTask = document.getElementById(`delete-${task.id}`)
+      svgDeleteTask.addEventListener("click", deleteTask);
+    });
    
   }
 
@@ -177,10 +185,9 @@ function renderTasks(){
   doneTasksRequest.onsuccess = function(e) {
 
     let doneTasks = e.target.result;
-    console.log(doneTasks);
 
     if (doneTasks.length === 0){
-      doneTasksContainer.innerHTML = "<p>¡No hay tareas pendientes!</p>"
+      doneTasksContainer.innerHTML = "<p>¡No hay tareas completadas!</p>"
     } else {
       doneTasks.forEach((task) => {
         let taskItem =  
@@ -197,17 +204,88 @@ function renderTasks(){
           </svg>
         </div>`
   
-        doneTasksContainer.innerHTML += taskItem
+        doneTasksContainer.innerHTML += taskItem;
       });
     }
+
+    doneTasks.forEach((task) => {
+      let svgDeleteTask = document.getElementById(`delete-${task.id}`)
+      svgDeleteTask.addEventListener("click", deleteTask);
+    });
    
   }
 
 }
 
+
+/* Función para agregar una nueva tarea */
+addTaskForm.addEventListener("submit", addNewtask);
+
+function addNewtask(e){
+
+  e.preventDefault();
+
+  //Obtenemos los valores enviados por el form
+  let taskName = addTaskForm.newTask;
+
+  //Creamos un objeto con los datos ingresados
+  let newTask = {
+    list_id: tasklistIndex,
+    task_name: taskName.value,
+    done_status: 0
+  }
+
+  //Agregamos el objeto a nuestra base de datos indexedDB
+  const transaction = db.transaction(['toDoTasks'], 'readwrite');
+  let objectStore = transaction.objectStore('toDoTasks');
+
+  let request = objectStore.add(newTask);
+
+  request.onsuccess = function() {
+
+    console.log('Se agregó la nueva tarea con éxito');
+
+    //Reseteamos los inputs del form
+    taskName.value = '';
+
+    //Renderizamos los nuevos valores
+    renderTaskCounters();
+    renderTasks();
+
+  }
+
+  request.onerror = function(event) {
+    console.log('Ocurrió un error intentando agregar la nueva tarea', event);
+  };
+
+}
+
+
+/* Función para eliminar una tarea en particular */
+function deleteTask(e){
+
+  let idSVG = e.currentTarget.id;
+  console.log(idSVG);
+
+  let taskID = parseInt(idSVG.replace('delete-',''));
+  console.log(taskID);
+
+  const transaction = db.transaction(['toDoTasks'], 'readwrite');
+  let objectStore = transaction.objectStore('toDoTasks');
+
+  let request = objectStore.delete(taskID);
+
+  request.onsuccess = function() {
+
+    console.log('Tarea eliminada con éxito');
   
+    //Renderizamos los nuevos valores
+    renderTaskCounters();
+    renderTasks();
 
+  }
 
+}
 
 
 
@@ -304,90 +382,6 @@ function deleteTasklist(){
 
 
 
-/* Ver las tareas (Página de detalle) */
-// function renderTasks(e){
-
-//   // Identificamos la lista 
-//   let idList = e.currentTarget.id;
-//   let index = arrayTaskLists.findIndex((taskList) => `${taskList.id}` === idList);
-
-//   // Guardamos en variables
-//   let selectedTaskList = arrayTaskLists[index];
-//   let tasks = arrayTaskLists[index].tasks;
-
-//   vaciarContainer();
-
-//   // Cambiamos el contenido del título de la página
-//   mainTitle.textContent = selectedTaskList.name;
-//   mainSubtitle.textContent = selectedTaskList.description;
-//   listType.textContent = selectedTaskList.type;
-
-//   /* Ver las tareas (por estado) */
-//   function showByStatus(){
-
-//     //Creamos 2 arrays nuevos a partir del array "tasks", usando el estado de sus tareas ("done_status")
-//     let pendingTasks = [];
-//     let doneTasks = [];
-
-//     tasks.forEach((task) => {
-//       if (task.done_status == false){
-//         pendingTasks.push(task);
-//       } else if (task.done_status == true){
-//         doneTasks.push(task);
-//       } else {
-//         console.log(task);
-//       }
-//     });
-
-//     //Columna "Tareas pendientes"
-//     let pendingTasksContainer = document.getElementById('tasksPending');
-
-//     if(pendingTasks.length === 0){
-//       pendingTasksContainer.innerHTML = "<p>¡No hay tareas pendientes!</p>"
-//     } else {
-//       pendingTasks.forEach((task) => {
-//         let taskItem =  
-//         `<div class="task">
-//           <input type="checkbox" id="${task.id}" class="task-checkbox">
-//           <label for="${task.id}"><span class="custom-checkbox"></span>${task.task_name}</label>
-//           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"
-//               viewbox="0 0 48 48" class="delete-list delete-list-task" id="delete-${task.id}">
-//               <path fill="#949BA3"
-//                   d="M10.742 13.543c-.686 0-1.242.634-1.242 1.413 0 .78.556 1.413 1.24 1.413h26.518c.686 0 1.242-.633 1.242-1.413s-.556-1.413-1.244-1.413h-4.14c-.854-.024-1.756-.642-2.046-1.554l-.049-.161-.183-.63-.007-.021c-.111-.38-.209-.71-.345-1.006-.543-1.19-1.553-2.017-2.718-2.228-.294-.053-.607-.053-.966-.053h-5.604c-.357 0-.67 0-.966.053-1.165.211-2.174 1.038-2.718 2.228-.138.3-.236.635-.347 1.018l-.003.009-.185.63-.049.16c-.29.913-1.042 1.53-1.894 1.555h-4.294Z" />
-//               <path fill="#949BA3" fill-rule="evenodd"
-//                   d="M24.65 40.112h-1.3c-4.485 0-6.726 0-8.184-1.427-1.457-1.425-1.606-3.766-1.904-8.441l-.43-6.757c-.163-2.54-.244-3.81.488-4.616.73-.805 1.966-.805 4.435-.805h12.49c2.47 0 3.704 0 4.435.805.73.804.65 2.076.488 4.616l-.43 6.756c-.298 4.676-.447 7.015-1.903 8.442-1.46 1.427-3.7 1.427-8.184 1.427Zm-4.797-16.945c.664-.07 1.257.44 1.321 1.139l.806 8.479c.066.7-.418 1.323-1.083 1.392-.664.07-1.257-.44-1.321-1.14l-.806-8.478c-.066-.7.418-1.323 1.083-1.392Zm9.377 1.392c.067-.7-.418-1.323-1.081-1.392-.663-.07-1.259.44-1.323 1.139l-.806 8.479c-.066.7.418 1.323 1.083 1.392.662.07 1.257-.44 1.321-1.14l.806-8.478Z"
-//                   clip-rule="evenodd" />
-//           </svg>
-//         </div>`
-  
-//         pendingTasksContainer.innerHTML += taskItem
-//       });
-//     }
-
-//     //Columna "Tareas completadas"
-//     let doneTasksContainer = document.getElementById('tasksDone');
-
-//     if(doneTasks.length === 0){
-//       doneTasksContainer.innerHTML = "<p>¡No hay tareas completadas!</p>"
-//     } else {
-//       doneTasks.forEach((task) => {
-//         let taskItem =  
-//         `<div class="task">
-//           <input type="checkbox" id="${task.id}" class="task-checkbox" checked>
-//           <label for="${task.id}"><span class="custom-checkbox"></span>${task.task_name}</label>
-//           <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="none"
-//               viewbox="0 0 48 48" class="delete-list delete-list-task" id="delete-${task.id}">
-//               <path fill="#949BA3"
-//                   d="M10.742 13.543c-.686 0-1.242.634-1.242 1.413 0 .78.556 1.413 1.24 1.413h26.518c.686 0 1.242-.633 1.242-1.413s-.556-1.413-1.244-1.413h-4.14c-.854-.024-1.756-.642-2.046-1.554l-.049-.161-.183-.63-.007-.021c-.111-.38-.209-.71-.345-1.006-.543-1.19-1.553-2.017-2.718-2.228-.294-.053-.607-.053-.966-.053h-5.604c-.357 0-.67 0-.966.053-1.165.211-2.174 1.038-2.718 2.228-.138.3-.236.635-.347 1.018l-.003.009-.185.63-.049.16c-.29.913-1.042 1.53-1.894 1.555h-4.294Z" />
-//               <path fill="#949BA3" fill-rule="evenodd"
-//                   d="M24.65 40.112h-1.3c-4.485 0-6.726 0-8.184-1.427-1.457-1.425-1.606-3.766-1.904-8.441l-.43-6.757c-.163-2.54-.244-3.81.488-4.616.73-.805 1.966-.805 4.435-.805h12.49c2.47 0 3.704 0 4.435.805.73.804.65 2.076.488 4.616l-.43 6.756c-.298 4.676-.447 7.015-1.903 8.442-1.46 1.427-3.7 1.427-8.184 1.427Zm-4.797-16.945c.664-.07 1.257.44 1.321 1.139l.806 8.479c.066.7-.418 1.323-1.083 1.392-.664.07-1.257-.44-1.321-1.14l-.806-8.478c-.066-.7.418-1.323 1.083-1.392Zm9.377 1.392c.067-.7-.418-1.323-1.081-1.392-.663-.07-1.259.44-1.323 1.139l-.806 8.479c-.066.7.418 1.323 1.083 1.392.662.07 1.257-.44 1.321-1.14l.806-8.478Z"
-//                   clip-rule="evenodd" />
-//           </svg>
-//         </div>`
-
-//         doneTasksContainer.innerHTML += taskItem
-//       });
-//     }
 
 //     //Se le agrega a cada tarea la función de cambiar el estado
 //     let inputCheckbox = document.querySelectorAll(".task-checkbox");
